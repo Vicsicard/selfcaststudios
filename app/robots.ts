12179d19acc961293df38e4982253d6c14e6c04a
vicsicard <vicsicard@gmail.com>
2025-04-02 14:01:38 -0600

    Fixed robots.ts format and sitemap path

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,16 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const siteUrl = process.env.SITE_URL || 'https://selfcaststudios.com'
+  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://selfcaststudios.com/server-sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   }
 }
