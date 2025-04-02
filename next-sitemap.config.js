a61f693137bddf7517628817d2bf305e0dcb0abf
vicsicard <vicsicard@gmail.com>
2025-04-02 14:13:40 -0600

    Added cache-busting comment to force Vercel rebuild

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
+// Force cache invalidation - 2025-04-02
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://selfcaststudios.com',
   generateRobotsTxt: true,
